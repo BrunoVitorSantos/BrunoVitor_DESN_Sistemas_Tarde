e2ce6e9d7154a94af89a91066c876c0ec4c00ea6
BrunoVitorSantos <brvisan@gmail.com>
2025-05-05 14:28:44 -0300

    Create test_cadastro_cliente.py

---
+++ test_cadastro_cliente.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+#
+driver = webdriver.Chrome()
+
+#
+#
+driver.get("C:\Users\bruno_vitor-santos\Documents\GitHub\BrunoVitor_DESN_Sistemas_Tarde\index.html")
