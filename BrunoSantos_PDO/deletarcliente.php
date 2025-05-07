176c0ec8464ba58fbca4b8ee4fe8dc3b9d99e325
BrunoVitorSantos <brvisan@gmail.com>
2025-05-07 17:07:54 -0300

    Add files via upload

---
+++ BrunoSantos_PDO/deletarcliente.php
@@ -0,0 +1,23 @@
+<?php
+
+// Inclui o arquivo de conexão com o banco de dados
+require_once 'conexao.php'; 
+
+// Chama a função para conectar ao banco de dados
+$conexao = conectadb(); 
+
+// ID do cliente a ser excluído
+$id_cliente = 4; 
+
+$stmt = $conexao->prepare("DELETE FROM cliente WHERE id_cliente = ?"); // Prepara a consulta SQL
+
+// Liga o parâmetro à consulta
+$stmt->bind_param("i", $id_cliente); 
+
+if ($stmt->execute()) { // Executa a consulta
+    echo "Cliente removido com sucesso!<br>";
+} else {
+    echo "Erro ao remover cliente: " . $stmt->error . "<br>";
+}
+
+?>
